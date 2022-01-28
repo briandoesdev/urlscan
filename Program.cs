99cabaeaf297fc62b2a53aed87cc1a6404091035
briandoesdev <brian.heidrich@icloud.com>
2022-01-27 18:01:02 -0600

    added http client

--- Program.cs
+++ Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Net.Http.Headers;
+namespace Urlscan;
+
+class Program {
+
+    private static HttpClient? http;
+
+    void Main() {
+
+    }
+
+    private static void InitiateHttpClient() {
+        http = new HttpClient() {
+            Timeout = TimeSpan.FromSeconds(20)
+        };
+
+        http.DefaultRequestHeaders.Accept.Clear();
+        http.DefaultRequestHeaders.Accept.Add(item: new MediaTypeWithQualityHeaderValue("application/json"));
+        http.DefaultRequestHeaders.Add(name: "User-Agent", value: "UrlScan-Cli/1.0.0");
+    }
+
+}
